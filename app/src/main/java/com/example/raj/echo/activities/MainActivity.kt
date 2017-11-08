b13f7ad63cf7bd5156a1675bf99688221c2c24e1
Rajdeep Sharma <rajdeep.sharma17@gmail.com>
2017-11-08 22:48:31 +0530

    Navigation Drawer

---
+++ app/src/main/java/com/example/raj/echo/activities/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.raj.echo.activities
+
+import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import com.example.raj.echo.R
+import com.example.raj.echo.adapters.NavigationDrawerAdapter
+import com.example.raj.echo.fragments.MainScreenFragment
+
+class MainActivity : AppCompatActivity(){
+
+    var navigationDrawerIconList: ArrayList<String> = arrayListOf()
+
+    var images_for_navdrawer = intArrayOf(R.drawable.navigation_allsongs, R.drawable.navigation_favorites,
+            R.drawable.navigation_settings, R.drawable.navigation_aboutus)
+
+    object Statified{
+        var drawerLayout: DrawerLayout? = null
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        MainActivity.Statified.drawerLayout = findViewById(R.id.drawer_layout)
+
+        navigationDrawerIconList.add("All Songs")
+        navigationDrawerIconList.add("Favorites")
+        navigationDrawerIconList.add("Settings")
+        navigationDrawerIconList.add("About Us")
+        val toggle = ActionBarDrawerToggle(this@MainActivity,MainActivity.Statified.drawerLayout,toolbar
+                , R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        MainActivity.Statified.drawerLayout?.setDrawerListener(toggle)
+
+        toggle.syncState()
+
+        val mainScreenFragment = MainScreenFragment()
+        this.supportFragmentManager
+                .beginTransaction()
+                .add(R.id.details_fragment,mainScreenFragment, "MainScreenFragment")
+                .commit()
+
+        var _navigationAdapter = NavigationDrawerAdapter(navigationDrawerIconList, images_for_navdrawer, this)
+        _navigationAdapter.notifyDataSetChanged()
+
+        var navigation_recycler_view = findViewById<RecyclerView>(R.id.navigation_recycler_view)
+        navigation_recycler_view.layoutManager = LinearLayoutManager(this)
+        navigation_recycler_view.itemAnimator = DefaultItemAnimator()
+        navigation_recycler_view.adapter = _navigationAdapter
+        navigation_recycler_view.setHasFixedSize(true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
